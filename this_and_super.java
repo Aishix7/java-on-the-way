17771a23a5bb28fa00a676203f2cdff35905143b
Aishix7 <hithaishi714@gmail.com>
2025-10-23 06:06:35 +0530

    here we've used this() method which class the constructor from same class only, and I've passed int value and it calls parameterized constructors, so that calls this() method and executes default contructor of same class as well

--- this_and_super.java
+++ this_and_super.java
@@ -1,26 +1,27 @@
 class A{
-    public A(){
-        super();
-        System.out.println("In default A");
-    }
-    public A(int n){
-        super();
-        System.out.println("In parameterized A");
-    }
+    // public A(){
+    //     super();
+    //     System.out.println("In default A");
+    // }
+    // public A(int n){
+    //     super();
+    //     System.out.println("In parameterized A");
+    // }
 }
 class B extends A{
     public B(){
-        super();
+        // super();
         System.out.println("In default B");
     }
     public B(int n){
-        super(n);
+        // super(n);
+        this();
         System.out.println("In parameterized B");
     }
 }
 public class this_and_super {
     public static void main(String[] args) {
-        B obj = new B();
-        B obj1 = new B(5);
+        B obj = new B(5);
+        // B obj1 = new B(5);
     }
 }
