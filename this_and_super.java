1094ab406bf503b8a28a2536b1d788eb694d18bc
Aishix7 <hithaishi714@gmail.com>
2025-10-23 05:54:52 +0530

    we have two classes where B extends A

---
+++ this_and_super.java
@@ -0,0 +1,21 @@
+class A{
+    public A(){
+        System.out.println("In default A");
+    }
+    public A(int n){
+        System.out.println("In parameterized A");
+    }
+}
+class B extends A{
+    public B(){
+        System.out.println("In default B");
+    }
+    public B(int n){
+        System.out.println("In parameterized B");
+    }
+}
+public class this_and_super {
+    public static void main(String[] args) {
+        B obj = new B();
+    }
+}
